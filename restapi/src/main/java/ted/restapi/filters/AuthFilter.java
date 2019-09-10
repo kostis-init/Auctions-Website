293e1c107ffcefc19893324dc52aff8962cb073d
kostis <kostismich@gmail.com>
2019-09-10 18:47:01 +0300

    fix: filter CORS issue

--- restapi/src/main/java/ted/restapi/filters/AuthFilter.java
+++ restapi/src/main/java/ted/restapi/filters/AuthFilter.java
@@ -20,19 +20,27 @@ public class AuthFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest reqHttp = (HttpServletRequest) request;
-        String jwt = reqHttp.getHeader("Authorization");
-        String username = JWT.getUsername(jwt);
-        if(username == null){
-            HttpServletResponse respHttp = (HttpServletResponse) response;
-            respHttp.setStatus(HttpServletResponse.SC_FORBIDDEN);
-            respHttp.getWriter().print("Invalid jwt");
-        } else{
-            chain.doFilter(request, response);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // Authorize (allow) all domains to consume the content
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
+
+        // For HTTP OPTIONS method reply with ACCEPTED status code -- per CORS handshake
+        if (((HttpServletRequest) request).getMethod().equals("OPTIONS")) {
+            ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
         }
 
+        String jwt = ((HttpServletRequest) request).getHeader("Authorization");
+        String username = JWT.getUsername(jwt);
+        if(username == null) {
+            ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().print("Invalid jwt");
+            return;
+        }
 
+        chain.doFilter(request, response);
     }
 
     @Override
