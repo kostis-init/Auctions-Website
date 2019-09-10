6ff59ac913a06de3c985e7625db3b3a44f8a93c5
kostis <kostismich@gmail.com>
2019-09-10 19:49:30 +0300

    add: extra CORS filter

--- restapi/src/main/java/ted/restapi/filters/AuthFilter.java
+++ restapi/src/main/java/ted/restapi/filters/AuthFilter.java
@@ -3,35 +3,16 @@ package ted.restapi.filters;
 import ted.restapi.util.JWT;
 
 import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {
-        "/api/categories/*",
-        "/api/items/*",
-        "/api/admin/*"
-})
 public class AuthFilter implements Filter {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
-    }
+    public void init(FilterConfig filterConfig) throws ServletException { }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        // Authorize (allow) all domains to consume the content
-        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
-        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
-
-        // For HTTP OPTIONS method reply with ACCEPTED status code -- per CORS handshake
-        if (((HttpServletRequest) request).getMethod().equals("OPTIONS")) {
-            ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_ACCEPTED);
-            return;
-        }
-
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String jwt = ((HttpServletRequest) request).getHeader("Authorization");
         String username = JWT.getUsername(jwt);
         if(username == null) {
@@ -39,12 +20,9 @@ public class AuthFilter implements Filter {
             response.getWriter().print("Invalid jwt");
             return;
         }
-
         chain.doFilter(request, response);
     }
 
     @Override
-    public void destroy() {
-
-    }
+    public void destroy() { }
 }
---
+++ restapi/src/main/java/ted/restapi/filters/CORSFilter.java
@@ -0,0 +1,29 @@
+package ted.restapi.filters;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException { }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // Authorize (allow) all domains to consume the content
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST, DELETE");
+
+        // For HTTP OPTIONS method reply with ACCEPTED status code -- per CORS handshake
+        if (((HttpServletRequest) request).getMethod().equals("OPTIONS")) {
+            ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() { }
+}
